9b5abfdf2ca8549397fed55f66899c20e9827b15
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-05-27 11:42:20 -0700

    finish deck import parsing

--- deck-import.js
+++ deck-import.js
@@ -1,8 +1,64 @@
 var path = require('path');
 var fs = require('fs');
 
-var file = path.join(__dirname, process.argv[2]);
-console.log(
-  file,
-  fs.readFileSync(file, 'utf8')
-);
+var filePath = path.join(__dirname, process.argv[2]);
+
+var deck = {
+  name: '',
+  cards: [
+  ]
+};
+
+var startedAt;
+var cardSections = [];
+
+fs.readFileSync(filePath, 'utf8').split('\n').forEach((line, no, file) => {
+  var title = line.match(/^# (.+)/);
+  if (title) {
+    deck.name = title[1];
+  }
+
+  if (line.match(/^## Card (\w+)/)) { // card section started
+    if (startedAt) {
+      throw Error('File could not be parsed.');
+    }
+    startedAt = no + 1;
+  }
+
+  if (line.match(/^-{3,}$/)) { // card section ended
+    if (!startedAt) {
+      throw Error('File could not be parsed.');
+    }
+    cardSections.push(file.slice(startedAt, no))
+    startedAt = null;
+  }
+});
+
+cardSections.forEach(cardSection => {
+  var card = {};
+  cardSection.forEach((line, no, section) => {
+    // card property found
+    var propStart = line.match(/^### (Question|Answer|Explanation+)/);
+    if (propStart) {
+      if(propStart[1] === 'Question') {
+        // mark the start of the question
+        startedAt = no + 1;
+      } else if (propStart[1] === 'Answer') {
+        // mark the end of a question and the start of an answer
+        card.question = {
+          text: section.slice(startedAt, no).join('\n')
+        };
+        startedAt = no + 1;
+      } else if (propStart[1] === 'Explanation') {
+        // mark the end of an answer and the rest is assumed to be explanation
+        card.answer = {
+          text: section.slice(startedAt, no).join('\n'),
+          explanation: section.slice(no + 1).join('\n'),
+        }
+      }
+    }
+  });
+  deck.cards.push(card);
+});
+
+fs.writeFileSync('deck.json', JSON.stringify(deck));
