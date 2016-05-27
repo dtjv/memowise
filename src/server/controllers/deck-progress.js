1dcaab9577b8ce677dd71dd790d919071f613083
Peter Chim <pchim24@gmail.com>
2016-05-26 21:59:59 -0700

    Clean up and export get card function

--- src/server/controllers/deck-progress.js
+++ src/server/controllers/deck-progress.js
@@ -1,3 +1,52 @@
+// query the database
+const queryDb = (id, queryString) => {
+  const queryAction = {
+    getDeck: () => dummyDecks[id],
+    getPlays: () => dummyPlays  // should get plays that match deck id (not implemented yet)
+  }
+  return queryAction[queryString]();
+};
+
+// given a deck, retrieve a card
+const getCard = deck_id => {
+  // must make these asyncronous
+  const deck = queryDb(deck_id, 'getDeck');
+  // get the plays that belong to the deck
+  let plays = queryDb(deck_id, 'getPlays');
+  // default: get any random card's index (changed below)
+  let rand_card_index = Math.floor(Math.random() * deck.cards.length);
+
+  let tries = 0;
+  const max_tries = 10;
+
+  // get a random card's index based on history of plays
+  // can make this into an async function
+  while (tries < max_tries) {
+    // get a random real number between 0 and 0.5
+    let rand_probability = Math.random() * 0.5;
+    // get a random card's index from the deck
+    let rand_card_index = Math.floor(Math.random() * deck.cards.length);
+    
+    // find all plays that match card id
+    let card_plays = plays.map(card => card.card_id === rand_card_index);
+    // get card's total rating
+    let total_rating = card_plays.reduce((total, card) => total + card.rating, 0);
+    // get a random real number based on card's total rating
+    let rand_card_probability = Math.random() * Math.pow(2, total_rating);
+    
+    // decide whether to show card based on the randomly generated numbers
+    if (rand_probability < rand_card_probability)
+      tries = max_tries;  // will break out of while loop
+
+    tries++;
+  }
+
+  return deck.cards[rand_card_index];
+}; 
+
+
+export default getCard;
+
 
 const dummyDecks = [
   { objId: 0,
@@ -58,56 +107,6 @@ const dummyPlays = [
     deck_id: 1,
     card_id: 1 
   }];
-
-
-// query the database
-const queryDb = (id, queryString) => {
-  const queryAction = {
-    getDeck: () => dummyDecks[id],
-    getPlays: () => dummyPlays  // should get plays that match deck id (not implemented yet)
-  }
-  return queryAction[queryString]();
-};
-
-// given a deck, retrieve a card
-const getCard = deck_id => {
-  // must make these asyncronous
-  const deck = queryDb(deck_id, 'getDeck');
-  // get the plays that belong to the deck
-  let plays = queryDb(deck_id, 'getPlays');
-  // default: get any random card's index (changed below)
-  let rand_card_index = Math.floor(Math.random() * deck.cards.length);
-
-  let tries = 0;
-  const max_tries = 10;
-
-  // get a random card's index based on history of plays
-  // can make this into an async function
-  while (tries < max_tries) {
-    // get a random real number between 0 and 0.5
-    let rand_probability = Math.random() * 0.5;
-    // get a random card's index from the deck
-    let rand_card_index = Math.floor(Math.random() * deck.cards.length);
-    
-    // find all plays that match card id
-    let card_plays = plays.map(card => card.card_id === rand_card_index);
-    // get card's total rating
-    let total_rating = card_plays.reduce((total, card) => total + card.rating, 0);
-    // get a random real number based on card's total rating
-    let rand_card_probability = Math.random() * Math.pow(2, total_rating);
-    
-    // decide whether to show card based on the randomly generated numbers
-    if (rand_probability < rand_card_probability)
-      tries = max_tries;  // will break out of while loop
-
-    tries++;
-  }
-
-  return deck.cards[rand_card_index];
-}; 
-
-console.log(getCard(1));
-
 
 
 
