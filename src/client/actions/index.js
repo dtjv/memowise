79af3625c17188c85b438b204f5ae78c475b5c69
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-05-31 19:15:25 -0700

    add play reducer/actions

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -3,6 +3,11 @@ import * as types from '../constants/actionTypes';
 export const receiveDeck = decks => ({ type: types.RECEIVE_DECKS, data: decks });
 export const selectDeck = deck => ({ type: types.SELECT_DECK, data: deck });
 export const receiveCard = card => ({ type: types.RECEIVE_CARD, data: card });
+
+export const startPlay = (cardId, deckId) => ({ type: 'START_PLAY', data: {cardId, deckId} });
+export const flipCard = () => ({ type: 'FLIP_CARD' });
+export const finishPlay = rating => ({ type: 'FINISH_PLAY', data: rating });
+
 export const failedRequest = error => ({ type: types.ERR_FAILED_REQUEST, data: error });
 
 export const fetchData = () => {
