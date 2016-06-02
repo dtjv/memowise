47766d6bca6191f13beb68733311612e49a0f4f3
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-02 13:50:33 -0700

    finish up auth bar component

--- src/client/components/Toolbar.js
+++ src/client/components/Toolbar.js
@@ -9,14 +9,8 @@ const Toolbar = () => (
           <a id="logo-container" href="/" className="brand-logo">
             <i className="material-icons">home</i>
           </a>
-          <ul className="right hide-on-med-and-down">
-            <li><a href="/signup">Sign Up</a></li>
-            <li><AuthBar /></li>
-          </ul>
-          <ul id="nav-mobile" className="side-nav">
-            <li><a href="/signup">Sign Up</a></li>
-            <li><AuthBar /></li>
-          </ul>
+          <AuthBar />
+          <AuthBar mobile />
           <a href="#" data-activates="nav-mobile" className="button-collapse">
             <i className="material-icons">menu</i>
           </a>
