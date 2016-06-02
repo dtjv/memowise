7e33813b0607ab28181ea82bf6f99c1a01a19ae0
Peter Chim <pchim24@gmail.com>
2016-06-02 03:10:13 -0700

    Initialize getProgress function

--- src/server/controllers/progress-bar.js
+++ src/server/controllers/progress-bar.js
@@ -1,10 +1,39 @@
-// import dbquery from './dbaccess.js';
+//import dbquery from './dbaccess.js';
+// query the database
+const queryDb = () => {
+  // given a deck id, get all the cards in that deck
+  const getDeck = (deckId) => {
+    return Cards.find({ deckId: deckId });
+  };
 
-// const getProgress = (inputDeck) {
-//   // input deck is either an id # or a deck object w/ an id property
-//   const deck_id = inputDeck._id === undefined ? inputDeck : inputDeck._id;
-//   // to query mongoose for plays and get distinct cards
-//   // const getcards = Plays.distinct('cardId', {deckId: deck_id}), (err, results) => {
-//   //   console.log(results);
-//   // });
-// };
+  // given a deck id and user id, get the plays for that deck
+  const getDistinctCardsPlays = (deckId, userId) => {
+    return Plays.distinct('cardId', {deckId: deck_id}); //, userId: userId });
+  };
+
+  const getCardPlays = (cardId, deckId, userId) => {
+    return Plays.find({ cardId: cardId, deckId: deckId }); //, userId: userId});
+  };
+
+  return ({
+    getDeck: getDeck,
+    getPlays: getPlays,
+    getCardPlays: getCardPlays
+  });
+};
+
+const getProgress = (deckId) => {
+  // input deck is either an id # or a deck object w/ an id property
+  // const deck_id = inputDeck._id === undefined ? inputDeck : inputDeck._id;
+  return queryDb().getDistinctCardsPlays(deckId, '0')
+    .then( distinctCards => {
+      console.log('dist cards: ', distinctCards);
+      return queryDb().getDeck(deckId).then( allCards => {
+        const progress = distinctCards.length / allCards.length;
+        console.log(progress);
+        return progress;
+      });
+    });
+};
+
+export default getProgress;
