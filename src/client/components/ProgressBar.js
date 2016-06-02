c3a1d74b7163cec0c340cf409c27cc50039be780
Peter Chim <pchim24@gmail.com>
2016-06-02 12:04:21 -0700

    Implement progress bar

--- src/client/components/ProgressBar.js
+++ src/client/components/ProgressBar.js
@@ -6,7 +6,7 @@ class ProgressBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      perc: '0%'
+      perc: '0%',
     };
     this.getProgress = this.getProgress.bind(this);
     this.deckId = this.props.id;
@@ -18,23 +18,22 @@ class ProgressBar extends React.Component {
 
   getProgress() {
     $.getJSON(`/api/progress/${this.deckId}`)
-      .done( perc => {
-        this.setState({ perc: (perc*100 + '%') });
+      .done(perc => {
+        this.setState({ perc: (`${perc * 100} + `) });
       });
   }
 
   render() {
-
     return (
       <div className="progress-bar">
         <div className="progress">
-          <div className="determinate" style={{width: this.state.perc }}>
+          <div className="determinate" style={{ width: this.state.perc }}>
           </div>
         </div>
       </div>
     );
   }
-};
+}
 
 ProgressBar.propTypes = {
   id: React.PropTypes.string.isRequired,
--- src/server/controllers/progress-bar.js
+++ src/server/controllers/progress-bar.js
@@ -1,40 +1,37 @@
 import Cards from '../../../dev/server/models/cards.js';
 import Plays from '../../../dev/server/models/plays.js';
 
-//import dbquery from './dbaccess.js';
+// import dbquery from './dbaccess.js';
 // query the database
 const queryDb = () => {
   // given a deck id, get all the cards in that deck
-  const getDeck = (deckId) => {
-    return Cards.find({ deckId: deckId });
-  };
+  const getDeck = deckId => Cards.find({ deckId });
 
+  // userId is currently set to '0' until we can obtain it globally or from a session
   // given a deck id and user id, get the plays for that deck
-  const getDistinctCardsPlayed = (deckId, userId) => {
-    return Plays.distinct('cardId', {deckId: deckId}); //, userId: userId });
-  };
-
-  const getCardPlays = (cardId, deckId, userId) => {
-    return Plays.find({ cardId: cardId, deckId: deckId }); //, userId: userId});
-  };
+  const getDistinctCardsPlayed = (deckId, userId) => (
+    Plays.distinct('cardId', { deckId, userId: '0' })
+  );
+  // userId is currently set to '0' until we can obtain it globally or from a session
+  const getCardPlays = (cardId, deckId, userId) => (
+    Plays.find({ cardId, deckId, userId: '0' })
+  );
 
   return ({
-    getDeck: getDeck,
-    getDistinctCardsPlayed: getDistinctCardsPlayed,
-    getCardPlays: getCardPlays
+    getDeck,
+    getDistinctCardsPlayed,
+    getCardPlays,
   });
 };
 
-const getProgress = (deckId) => {
-  // input deck is either an id # or a deck object w/ an id property
-  // const deck_id = inputDeck._id === undefined ? inputDeck : inputDeck._id;
-  return queryDb().getDistinctCardsPlayed(deckId, '0')
-    .then( distinctCards => {
-      return queryDb().getDeck(deckId).then( allCards => {
+const getProgress = deckId => (
+  queryDb().getDistinctCardsPlayed(deckId, '0')
+    .then(distinctCards => (
+      queryDb().getDeck(deckId).then(allCards => {
         const progress = distinctCards.length / allCards.length;
         return progress;
-      });
-    });
-};
+      })
+    ))
+);
 
 export default getProgress;
