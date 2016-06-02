d456ac38848e99f1bbf57a9f650417451b4e8080
David Valles <davidvalles@yahoo.com>
2016-06-02 08:13:58 -0700

    Comment out. Not used.

--- src/server/controllers/progress-bar.js
+++ src/server/controllers/progress-bar.js
@@ -1,10 +1,10 @@
-import dbquery from './dbaccess.js';
+// import dbquery from './dbaccess.js';
 
-const getProgress = (inputDeck) {
-  // input deck is either an id # or a deck object w/ an id property
-  const deck_id = inputDeck._id === undefined ? inputDeck : inputDeck._id;
-  // to query mongoose for plays and get distinct cards
-  // const getcards = Plays.distinct('cardId', {deckId: deck_id}), (err, results) => {
-  //   console.log(results);
-  // });
-};
+// const getProgress = (inputDeck) {
+//   // input deck is either an id # or a deck object w/ an id property
+//   const deck_id = inputDeck._id === undefined ? inputDeck : inputDeck._id;
+//   // to query mongoose for plays and get distinct cards
+//   // const getcards = Plays.distinct('cardId', {deckId: deck_id}), (err, results) => {
+//   //   console.log(results);
+//   // });
+// };
