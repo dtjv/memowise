8dd3c2ca4d831d1b3e1e5e361513885039fa2b54
David Valles <davidvalles@yahoo.com>
2016-06-02 21:06:04 -0700

    Flip through cards in a deck and persist plays in db.

--- src/client/containers/StudyDeck.js
+++ src/client/containers/StudyDeck.js
@@ -1,21 +1,22 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchCard, startPlay, flipCard, finishPlay } from '../actions';
+import { GREAT, OKAY, BAD } from '../constants/play';
+import { fetchCard, startPlay, flipCard, savePlay } from '../actions';
 
 const mapStateToProps = ({ deck, card, play }) => ({ deck, card, play });
 
 const mapDispatchToProps = (dispatch) => ({
-  getCard: (deck) => dispatch(fetchCard(deck)),
-  setPlayStart: (cardId, deckId) => dispatch(startPlay(cardId, deckId)),
-  setPlayFlipCard: () => dispatch(flipCard()),
-  setPlayFinish: (rating) => dispatch(finishPlay(rating)),
+  fetchCard: (deck) => dispatch(fetchCard(deck)),
+  flipCard: () => dispatch(flipCard()),
+  startPlay: (cardId, deckId) => dispatch(startPlay(cardId, deckId)),
+  savePlay: (play, rating) => dispatch(savePlay(play, rating)),
 });
 
 class StudyDeck extends React.Component {
   constructor(props) {
     super(props);
     this.loadCard = this.loadCard.bind(this);
-    this.completePlay = this.completePlay.bind(this);
+    this.handlePlay = this.handlePlay.bind(this);
   }
 
   componentWillMount() {
@@ -23,33 +24,31 @@ class StudyDeck extends React.Component {
   }
 
   loadCard() {
-    const props = this.props;
-    props.getCard(props.deck._id)
-      .then(({ data }) => props.setPlayStart(data._id, data.deckId));
+    this.props.fetchCard(this.props.deck._id)
+      .then(({ data }) => this.props.startPlay(data._id, data.deckId));
   }
 
-  // i have not persisted plays!!
-  completePlay(event) {
-    const el = event.target;
-    const rating = el.dataset.rating || 0;
-    this.props.setPlayFinish(rating)
+  handlePlay(play, rank) {
+    this.props.savePlay(play, rank)
       .then(() => this.loadCard());
   }
 
   showCardFront() {
-    const { card } = this.props;
+    const { card: { question } } = this.props;
     return (
       <section>
         <br />
         <br />
         <br />
-        <h4>{(card.question && card.question.text) || null}</h4>
+        <h4>
+          {(question && question.text) || null}
+        </h4>
         <br />
         <br />
         <br />
         <div>
           <button
-            onClick={this.props.setPlayFlipCard}
+            onClick={this.props.flipCard}
             className="btn btn-large blue lighten-2"
           > Flip </button>
         </div>
@@ -58,26 +57,35 @@ class StudyDeck extends React.Component {
   }
 
   showCardBack() {
-    const { card } = this.props;
+    const { card: { answer }, play } = this.props;
     return (
       <section>
         <br />
         <br />
         <br />
-        <h4>{(card.answer && card.answer.text) || null}</h4>
+        <h4>{answer.text || null}</h4>
         <br />
         <br />
         <br />
-        <p>{(card.answer && card.answer.explanation) || null}</p>
+        <p>{answer.explanation || null}</p>
         <div>
-          <button onClick={this.completePlay} className="btn btn-large blue lighten-2">
-            <i ref="play-bad" data-rating="-1" className="material-icons">thumb_down</i>
+          <button
+            onClick={() => this.handlePlay(play, BAD)}
+            className="btn btn-large blue lighten-2"
+          >
+            <i className="material-icons">thumb_down</i>
           </button>
-          <button onClick={this.completePlay} className="btn btn-large blue lighten-2">
-            <i ref="play-okay" data-rating="0" className="material-icons">help</i>
+          <button
+            onClick={() => this.handlePlay(play, OKAY)}
+            className="btn btn-large blue lighten-2"
+          >
+            <i className="material-icons">help</i>
           </button>
-          <button onClick={this.completePlay} className="btn btn-large blue lighten-2">
-            <i ref="play-good" data-rating="1" className="material-icons">thumb_up</i>
+          <button
+            onClick={() => this.handlePlay(play, GREAT)}
+            className="btn btn-large blue lighten-2"
+          >
+            <i className="material-icons">thumb_up</i>
           </button>
         </div>
       </section>
@@ -85,12 +93,12 @@ class StudyDeck extends React.Component {
   }
 
   render() {
-    const { play, deck } = this.props;
+    const { play: { side }, deck } = this.props;
     return (
       <div className="container">
         <h2 className="center grey-text text-darken-4">{deck.name}</h2>
         <div className="card medium center">
-          {!play.side ? this.showCardFront() : this.showCardBack()}
+          {!side ? this.showCardFront() : this.showCardBack()}
         </div>
       </div>
     );
@@ -101,10 +109,10 @@ StudyDeck.propTypes = {
   deck: PropTypes.object.isRequired,
   card: PropTypes.object.isRequired,
   play: PropTypes.object.isRequired,
-  getCard: PropTypes.func.isRequired,
-  setPlayStart: PropTypes.func.isRequired,
-  setPlayFlipCard: PropTypes.func.isRequired,
-  setPlayFinish: PropTypes.func.isRequired,
+  fetchCard: PropTypes.func.isRequired,
+  flipCard: PropTypes.func.isRequired,
+  startPlay: PropTypes.func.isRequired,
+  savePlay: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(StudyDeck);
