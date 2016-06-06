1dbabd31bf6ce56ce84ac51211d22a7c0ba50e86
Peter Chim <pchim24@gmail.com>
2016-06-06 15:57:36 -0700

    Check log-in authorization on server side

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -10,15 +10,15 @@ const router = new Router();
 /*
  * Decks
  */
-router.route('/api/decks').get(decks.findAll);
-router.route('/api/card').post(decks.findNextCard);
-router.route('/api/progress').post(decks.progress);
+router.route('/api/decks').get(auth.checkAuthServer, decks.findAll);
+router.route('/api/card').post(auth.checkAuthServer, decks.findNextCard);
+router.route('/api/progress').post(auth.checkAuthServer, decks.progress);
 
 /*
  * Plays
  */
-router.route('/api/play').post(plays.create);
-router.route('/api/last-play/deck/:deckId').get(plays.findLatest);
+router.route('/api/play').post(auth.checkAuthServer, plays.create);
+router.route('/api/last-play/deck/:deckId').get(auth.checkAuthServer, plays.findLatest);
 
 /*
  * Auth
