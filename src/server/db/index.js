d65a55638deb92d4f3aac563d5bd125525284e02
David Valles <davidtjvalles@gmail.com>
2017-01-24 19:20:30 -0800

    Add native promise to mongoose

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { DB_NAME, DB_HOST, DB_PORT } = require('../../config');
 
+mongoose.Promise = global.Promise;
+
 if (!mongoose.connection.readyState) {
   mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
 }
