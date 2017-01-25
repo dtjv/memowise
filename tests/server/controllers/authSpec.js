adc6530c95b3b56d2f783c8a6346de494034f88b
David Valles <davidtjvalles@gmail.com>
2017-01-24 19:20:54 -0800

    Make auth controller tests pass

--- tests/server/controllers/authSpec.js
+++ tests/server/controllers/authSpec.js
@@ -1,16 +1,14 @@
-/* global describe, it, before, beforeEach, after, afterEach */
+/* global describe, xdescribe, it, before, beforeEach, after, afterEach */
 
-import chai from 'chai';
+import { expect } from 'chai';
 import request from 'supertest';
 import mongoose from '../../../src/server/db';
-
-// should be replaced by just loading fixtures before each test
 import User from '../../../src/server/models/User';
+import { HOST, PORT, PROTOCOL } from '../../../src/config';
 
-const expect = chai.expect;
+const baseUrl = `${PROTOCOL}://${HOST}:${PORT}`;
 
-describe('Routing', () => {
-  const url = 'http://localhost:3000';
+describe('Auth Routing', () => {
   const user = {
     name: 'Tester Name',
     email: 'name@tester.com',
@@ -27,18 +25,18 @@ describe('Routing', () => {
 
   describe('Account', () => {
     it('should return error trying to save duplicate username', (done) => {
-      request(url)
+      request(baseUrl)
         .post('/api/auth/create-account')
         .send(user)
         .expect(201)
         .end((err, res) => {
           if (err) {
-            throw err;
+            return done(err);
           }
           expect(res.body.name).to.equal(user.name);
           expect(res.body.email).to.equal(user.email);
           expect(res.body.password).to.equal(undefined);
-          done();
+          return done();
         });
     });
   });
@@ -46,48 +44,38 @@ describe('Routing', () => {
   describe('Authentication', () => {
     it('should fail to sign in with bad credentials', (done) => {
       User.create(user)
-      .then(() => {
-        request(url)
-          .post('/api/auth/sign-in')
-          .send({ email: user.email, password: 'incorrect' })
-          .expect(401)
-          .end((err) => {
-            if (err) {
-              throw err;
-            }
-            done();
-          });
-      });
+        .then(() => {
+          request(baseUrl)
+            .post('/api/auth/sign-in')
+            .send({ email: user.email, password: 'incorrect' })
+            .expect(401)
+            .end(err => (err ? done(err) : done()));
+        });
     });
 
     it('should return the user profile on successful sign in', (done) => {
       User.create(user)
-      .then(() => {
-        request(url)
-          .post('/api/auth/sign-in')
-          .send({ email: user.email, password: user.password })
-          .expect(200)
-          .end((err, res) => {
-            if (err) {
-              throw err;
-            }
-            expect(res.body.name).to.equal(user.name);
-            expect(res.body.email).to.equal(user.email);
-            done();
-          });
-      });
+        .then(() => {
+          request(baseUrl)
+            .post('/api/auth/sign-in')
+            .send({ email: user.email, password: user.password })
+            .expect(200)
+            .end((err, res) => {
+              if (err) {
+                done(err);
+              }
+              expect(res.body.name).to.equal(user.name);
+              expect(res.body.email).to.equal(user.email);
+              done();
+            });
+        });
     });
 
     it('should fail to verify athentication when not signed in', (done) => {
-      request(url)
+      request(baseUrl)
         .get('/api/auth/verify')
         .expect(401)
-        .end((err) => {
-          if (err) {
-            throw err;
-          }
-          done();
-        });
+        .end(err => (err ? done(err) : done()));
     });
 
     it('should verify athentication when signed in', (done) => {
@@ -95,35 +83,35 @@ describe('Routing', () => {
 
       // used to verify after setup
       const verify = () => {
-        const req = request(url).get('/api/auth/verify');
+        const req = request(baseUrl).get('/api/auth/verify');
 
         req.cookies = session;
         req.expect(200)
-        .end((err, res) => {
-          if (err) {
-            throw err;
-          }
-          expect(res.body.name).to.equal(user.name);
-          expect(res.body.email).to.equal(user.email);
-          done();
-        });
+          .end((err, res) => {
+            if (err) {
+              return done(err);
+            }
+            expect(res.body.name).to.equal(user.name);
+            expect(res.body.email).to.equal(user.email);
+            return done();
+          });
       };
 
       // setup user and sign them in
       User.create(user)
-      .then(() => {
-        request(url)
-          .post('/api/auth/sign-in')
-          .send({ email: user.email, password: user.password })
-          .expect(200)
-          .end((err, res) => {
-            if (err) {
-              throw err;
-            }
-            session = res.headers['set-cookie'].pop().split(';')[0];
-            verify();
-          });
-      });
+        .then(() => {
+          request(baseUrl)
+            .post('/api/auth/sign-in')
+            .send({ email: user.email, password: user.password })
+            .expect(200)
+            .end((err, res) => {
+              if (err) {
+                return done(err);
+              }
+              session = res.headers['set-cookie'].pop().split(';')[0];
+              return verify();
+            });
+        });
     });
   });
 });
