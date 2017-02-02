8d7e1c1145ce47b02c5904a705753d7e62d3e897
David Valles <davidtjvalles@gmail.com>
2017-02-02 10:14:04 -0800

    Add redux store persistance

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,16 +1,22 @@
 // redux modules
 import thunk from 'redux-thunk';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { asyncSessionStorage } from 'redux-persist/storages';
 import * as reducers from '../reducers';
+
+const enhancer = compose(applyMiddleware(thunk), autoRehydrate());
 
 export const store = createStore(
   combineReducers({
     ...reducers,
     routing: routerReducer,
   }),
-  applyMiddleware(thunk),
+  enhancer,
 );
+
+persistStore(store, { storage: asyncSessionStorage, blacklist: ['routing'] });
 
 export const history = syncHistoryWithStore(browserHistory, store);
