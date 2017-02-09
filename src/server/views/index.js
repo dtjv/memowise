8d5d5d5eb4e5b9294731ae8372cebe8e316c61f7
David Valles <davidtjvalles@gmail.com>
2017-02-08 22:17:22 -0800

    Fix malfunction of mobile slider

--- src/server/views/index.js
+++ src/server/views/index.js
@@ -10,10 +10,10 @@ module.exports = `
       <div id="app"></div>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
       <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+      <script type="text/javascript" src="bundle.js"></script>
       <script>
         $('.button-collapse').sideNav();
       </script>
-      <script type="text/javascript" src="bundle.js"></script>
     </body>
   </html>
 `;
