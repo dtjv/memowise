939137de88be46bf145a5a47093fbc26c98eca3a
David Valles <davidtjvalles@gmail.com>
2020-11-30 18:49:26 -0800

    Add a Section and Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,8 @@
-import { useAuth0 } from "@auth0/auth0-react";
-
-export function Nav() {
-  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
-
-  console.log(user, "<-- user");
-
-  return (
-    <nav>
-      <h1>memowise</h1>
-
-      {isAuthenticated ? (
-        <div>
-          <span
-            id="hello"
-            onClick={() => logout({ returnTo: window.location.origin })}
-          >
-            Hello, {user.name}!
-          </span>{" "}
-        </div>
-      ) : (
-        <div>
-          <button id="login" onClick={() => loginWithRedirect()}>
-            login
-          </button>
-        </div>
-      )}
-    </nav>
-  );
-}
+export const Nav = () => (
+  <nav>
+    <div className="flex items-center justify-between py-6 border-b border-gray-200">
+      <div>memowise</div>
+      <div>menu</div>
+    </div>
+  </nav>
+);
