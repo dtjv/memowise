f271b027edb8fd2a15b340316c32cce659ee1d34
David Valles <davidtjvalles@gmail.com>
2020-12-09 18:44:49 -0800

    Make demo persist Topics, CardSets

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,47 @@
 const fs = require('fs')
 const mongoose = require('mongoose')
 
+/*
+const userSchema = new mongoose.Schema({
+  name: String,
+  sets: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'CardSet',
+    },
+  ],
+})
+*/
+
+const topicSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  subTopics: [
+    {
+      name: String,
+      description: String,
+      numSets: Number,
+    },
+  ],
+})
+
+const cardSetSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  topic: mongoose.Schema.Types.ObjectId,
+  subTopic: mongoose.Schema.Types.ObjectId,
+  cards: [
+    {
+      term: String,
+      definition: String,
+    },
+  ],
+})
+
+//const User = mongoose.model('User', userSchema)
+const Topic = mongoose.model('Topic', topicSchema)
+const CardSet = mongoose.model('CardSet', cardSetSchema)
+
 const MARKER = '__MEMOWISE__'
 
 const parseEnvFile = (data) => {
@@ -44,7 +85,73 @@ const main = async () => {
   }
   console.log('connected to db')
 
-  // proceed....
+  /*
+  await Topic.create([
+    {
+      name: 'Math',
+      description: 'Learn all things math!',
+      subtopics: [
+        {
+          name: 'Algebra',
+          description: 'A few basic algebra flashcards sets.',
+          numSets: 0,
+        },
+        {
+          name: 'Geometry',
+          description: 'Learn geometry concepts like area and circumference.',
+          numSets: 0,
+        },
+      ],
+    },
+    {
+      name: 'Languages',
+      description: 'The fastest way to being proficient in a foreign language.',
+      subtopics: [
+        {
+          name: 'Spanish',
+          description: 'Buenos Dias!',
+          numSets: 0,
+        },
+        {
+          name: 'Italian',
+          description: 'Learn Italian in 3 days!',
+          numSets: 0,
+        },
+      ],
+    },
+  ])
+  */
+
+  const topic = await Topic.findOne({ name: 'Math' })
+  const subTopic = topic.subTopics.find(
+    (subTopic) => subTopic.name === 'Algebra'
+  )
+
+  const math = new CardSet({
+    name: 'My Algebra Set',
+    description: 'A few Algebra refresher cards',
+    topic: topic.id,
+    subTopic: subTopic.id,
+    cards: [
+      {
+        term: 'variable',
+        definition: 'A symbol or letter used to represent an unknown value.',
+      },
+      {
+        term: 'constant',
+        definition: 'A number by itself.',
+      },
+      {
+        term: 'co-efficient',
+        definition: 'A number that is multiplied by the value of a variable.',
+      },
+    ],
+  })
+  await math.save()
+
+  console.log(math.inspect())
+
+  process.exit(0)
 }
 
 main()
