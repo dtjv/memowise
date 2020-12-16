836885c1de1b135a2d66797671b80037ebad20ca
David Valles <davidtjvalles@gmail.com>
2020-12-16 09:58:09 -0800

    Remove Auth0 code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,7 @@
-//import "tailwindcss/tailwind.css"
-import Router from 'next/router'
-import { Auth0Provider } from '@auth0/auth0-react'
-
 import '../styles/app.css'
 
-const onRedirectCallback = (appState) => {
-  Router.replace(appState?.returnTo || '/')
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_DOMAIN}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID}
-      scope="read:users"
-      redirectUri={typeof window !== 'undefined' && window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <Component {...pageProps} />
-    </Auth0Provider>
-  )
-}
-
-export default MyApp
+export default App
