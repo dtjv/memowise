53b7fc4dda080a30184f6d67739840b412ccb996
David Valles <davidtjvalles@gmail.com>
2021-01-03 11:29:28 -0800

    Add links to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,28 +4,28 @@ export const Footer = () => {
   return (
     <footer className="py-12 mt-8 border-t bg-gray-50">
       <div className="max-w-3xl px-4 mx-auto antialiased sm:px-8 md:px-12 lg:px-0 divide-y divide-gray-200">
-        <ul className="pb-12 grid grid-cols-2 sm:grid-cols-3 gap-y-10">
+        <ul className="pb-12 grid gap-5 grid-cols-1 sm:grid-cols-3 gap-y-10">
           <li className="space-y-5">
             <h4 className="font-semibold text-gray-500 uppercase">Topics</h4>
             <ul className="text-gray-500 space-y-4">
               <li>
                 <Link href="/browse/math">
-                  <a> Math </a>
+                  <a className="hover:text-blue-500"> Math </a>
                 </Link>
               </li>
               <li>
                 <Link href="/browse/languages">
-                  <a> Languages </a>
+                  <a className="hover:text-blue-500"> Languages </a>
                 </Link>
               </li>
               <li>
                 <Link href="/browse/science">
-                  <a> Science </a>
+                  <a className="hover:text-blue-500"> Science </a>
                 </Link>
               </li>
               <li>
                 <Link href="/browse/all-the-things">
-                  <a> All The Things </a>
+                  <a className="hover:text-blue-500"> All The Things </a>
                 </Link>
               </li>
             </ul>
@@ -33,23 +33,55 @@ export const Footer = () => {
           <li className="space-y-5">
             <h4 className="font-semibold text-gray-500 uppercase">Features</h4>
             <ul className="text-gray-500 space-y-4">
-              <li>Flashcard Study</li>
-              <li>Self Test</li>
-              <li>Create Flashcards</li>
-              <li>Personalized Collection</li>
+              <li>
+                <Link href="/features#browse">
+                  <a className="hover:text-blue-500">
+                    Browse Topics & Flashcards
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/features#quiz">
+                  <a className="hover:text-blue-500">Flashcard Quiz</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/features#create">
+                  <a className="hover:text-blue-500">Create Flashcards</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/features#personalize">
+                  <a className="hover:text-blue-500">Personalized Collection</a>
+                </Link>
+              </li>
             </ul>
           </li>
           <li className="space-y-5">
             <h4 className="font-semibold text-gray-500 uppercase">About</h4>
             <ul className="text-gray-500 space-y-4">
-              <li>How I Built MemoWise</li>
-              <li>Study Algorithm</li>
+              <li>
+                <Link href="/about#how">
+                  <a className="hover:text-blue-500">How I Built MemoWise</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about#algorithm">
+                  <a className="hover:text-blue-500">Study Algorithm</a>
+                </Link>
+              </li>
             </ul>
           </li>
         </ul>
         <div className="pt-8 text-center">
           <p className="text-base font-medium text-gray-500">
-            Made with ☕️☕️☕️ by David Valles
+            Made with ☕️☕️☕️ by{' '}
+            <a
+              href="https://dtjv.io"
+              className="font-semibold text-gray-900 hover:text-blue-500"
+            >
+              David Valles
+            </a>
           </p>
         </div>
       </div>
