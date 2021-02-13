33acaf86e564ad6b9a3f8439168a9662784a08e2
David Valles <davidtjvalles@gmail.com>
2021-02-13 04:40:51 -0800

    Enable GitHub auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/app.css'
+import { Provider } from 'next-auth/client'
 
-function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default App
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  providers: [
+    /*
+    Providers.Email({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    */
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  //  database: process.env.DB_URI,
+  secret: process.env.SECRET,
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    encryption: true,
+  },
+  pages: {},
+  callbacks: {},
+  events: {},
+  debug: false,
+})
