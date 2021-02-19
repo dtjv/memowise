30eb9f6d2c8fd923bc2aca1bde9c5c64cd43477a
David Valles <davidtjvalles@gmail.com>
2021-02-19 12:02:47 -0800

    Submit new deck form to api [wip]

---
+++ src/pages/api/decks/index.js
@@ -0,0 +1,16 @@
+//import { Topic } from '@/models/Topic'
+//import { Deck } from '@/models/Deck'
+//import { connectToDB } from '@/utils/connectToDB'
+//import { transformObjectId } from '@/utils/transformObjectId'
+import { dump } from '@/utils/debug'
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    // TODO: check session
+
+    dump(req.body.deck)
+    return res.status(200).send('success')
+  }
+
+  res.status(404).send('unsupported method')
+}
--- src/pages/decks/new.js
+++ src/pages/decks/new.js
@@ -1,9 +1,11 @@
 import { inspect } from 'util'
 import { useState } from 'react'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 //import { useSession } from 'next-auth/client'
 import { v4 as uuid } from 'uuid'
 
+import { dump } from '@/utils/debug'
 import { Layout } from '@/components/Layout'
 import { Container } from '@/components/Container'
 
@@ -42,7 +44,9 @@ const NewCard = ({ card, onChange, onDelete }) => (
 )
 
 const NewDeckPage = () => {
-  //const [title, setTitle] = useState('')
+  const router = useRouter()
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
   const [cards, setCards] = useState([])
 
   console.log(inspect(cards, { depth: 4, colors: true }))
@@ -69,6 +73,36 @@ const NewDeckPage = () => {
     setCards((cardList) => cardList.filter((card) => card.id !== cardId))
   }
 
+  const handleCreateDeck = async () => {
+    const deck = {
+      name,
+      description,
+      creator: '', // pulled from session
+      topicId: '', // swr. show in drop-down. optional.
+      subTopicId: '', // swr. show in drop-down. optional.
+      // strip id fields out... they were for rendering purposes only.
+      cards: cards.map((card) => ({
+        term: card.term,
+        definition: card.definition,
+      })),
+    }
+
+    const res = await fetch(`/api/decks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ deck }),
+    })
+
+    if (!res.ok) {
+      dump(await res.text())
+      return
+    }
+
+    router.push('/')
+  }
+
   return (
     <Layout>
       <Head>
@@ -77,18 +111,20 @@ const NewDeckPage = () => {
       <Container>
         <p>Create a new flashcard deck</p>
         <label className="block">
-          Title
+          Name
           <input
             type="text"
-            name="title"
-            placeholder="Add a title (i.e. Math 101)"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Add a name (i.e. Math 101)"
             className="block w-full"
           />
         </label>
         <label className="block">
           Description
           <textarea
-            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
             placeholder="Add a description"
             className="block w-full"
             rows="3"
@@ -116,6 +152,7 @@ const NewDeckPage = () => {
         <button
           className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
           aria-label="add card"
+          onClick={handleCreateDeck}
         >
           Create
         </button>
