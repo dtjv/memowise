a02abad0fe37e8927500e86844cd7c6f900d1ad3
David Valles <davidtjvalles@gmail.com>
2021-02-20 19:02:58 -0800

    Add 2 decks w/o a topic/subtopic

---
+++ src/data/deck.js
@@ -0,0 +1,44 @@
+exports.data = {
+  decks: [
+    {
+      name: 'Spanish Verbs',
+      description:
+        'A few Spanish verb, and if I remember, their conjugated forms',
+      cards: [
+        {
+          term: 'hacer',
+          definition:
+            'to make. yo hago. tu haces. el/ella hace. nosotros hacemos. ellos/ellas hacen',
+        },
+        {
+          term: 'tener',
+          definition:
+            'to want. yo tengo. tu tienes. el/ella tiene. nosotros tenemos. ellos/ellas tienen.',
+        },
+        {
+          term: 'ir',
+          definition:
+            'to go. yo voy. tu vas. el/ella va. nosotros vamos. ellos/ellas van.',
+        },
+      ],
+    },
+    {
+      name: 'English Vocabulary 101',
+      description: 'A few English words for SAT study.',
+      cards: [
+        {
+          term: 'abate',
+          definition: 'to become less active, less intense, or less in amount',
+        },
+        {
+          term: 'assertion',
+          definition: 'a strong declaration',
+        },
+        {
+          term: 'avenue',
+          definition: 'an intangible path or approach to something',
+        },
+      ],
+    },
+  ],
+}
