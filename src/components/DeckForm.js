277f462abb7528918f3f338c1ef43d6a01b51cdc
David Valles <davidtjvalles@gmail.com>
2021-02-25 15:02:15 -0800

    Refactor create/edit deck forms

---
+++ src/components/DeckForm.js
@@ -0,0 +1,112 @@
+import { useState } from 'react'
+import { v4 as uuid } from 'uuid'
+
+import { Container } from '@/components/Container'
+import { TrashCanIcon } from '@/components/icons/trash-can'
+
+const createEmptyCard = () => ({ __uid: uuid(), term: '', definition: '' })
+
+const CardsForm = ({ cards, onChange, onDelete }) => {
+  return (
+    <ul>
+      {cards.map((card) => (
+        <li key={card.__uid}>
+          <label className="block">
+            Term
+            <input
+              type="text"
+              value={card.term}
+              onChange={(e) => onChange(card.__uid, 'term', e.target.value)}
+              placeholder="Add term"
+              className="block w-full"
+            />
+          </label>
+          <label className="block">
+            Definition
+            <textarea
+              value={card.definition}
+              onChange={(e) =>
+                onChange(card.__uid, 'definition', e.target.value)
+              }
+              placeholder="Add definition"
+              className="block w-full"
+              rows="1"
+            ></textarea>
+          </label>
+          <button onClick={() => onDelete(card.__uid)}>
+            <TrashCanIcon className="w-6 h-6" />
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export const DeckForm = ({ deck = {}, submitLabel, onSubmit }) => {
+  const [name, setName] = useState(deck.name ?? '')
+  const [description, setDescription] = useState(deck.description ?? '')
+  const [cards, setCards] = useState(deck.cards ?? [])
+
+  const handleChangeCard = (cardId, field, newValue) => {
+    setCards((cardList) =>
+      cardList.map((card) =>
+        card.__uid === cardId ? { ...card, [field]: newValue } : card
+      )
+    )
+  }
+
+  const handleAddCard = () => {
+    setCards((cardList) => [createEmptyCard(), ...cardList])
+  }
+
+  const handleDeleteCard = (cardId) => {
+    setCards((cardList) => cardList.filter((card) => card.__uid !== cardId))
+  }
+
+  return (
+    <Container>
+      <p>Create a new flashcard deck</p>
+      <label className="block">
+        Name
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Add a name (i.e. Math 101)"
+          className="block w-full"
+        />
+      </label>
+      <label className="block">
+        Description
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Add a description"
+          className="block w-full"
+          rows="3"
+        ></textarea>
+      </label>
+      <button
+        onClick={handleAddCard}
+        className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        aria-label="add card"
+      >
+        Add Card
+      </button>
+
+      <CardsForm
+        cards={cards}
+        onChange={handleChangeCard}
+        onDelete={handleDeleteCard}
+      />
+
+      <button
+        className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+        aria-label="update deck"
+        onClick={() => onSubmit({ name, description, cards })}
+      >
+        {submitLabel}
+      </button>
+    </Container>
+  )
+}
--- src/pages/decks/create.js
+++ src/pages/decks/create.js
@@ -1,82 +1,23 @@
-import { useState } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/client'
-import { v4 as uuid } from 'uuid'
 import axios from 'axios'
 
-import { Container } from '@/components/Container'
-
-//import { dump } from '@/utils/debug'
-
-const createEmptyCard = () => ({ __uid: uuid(), term: '', definition: '' })
-
-const NewCard = ({ card, onChange, onDelete }) => (
-  <li>
-    <label className="block">
-      Term
-      <input
-        type="text"
-        value={card.term}
-        onChange={(e) => onChange(card.__uid, 'term', e.target.value)}
-        placeholder="Add term"
-        className="block w-full"
-      />
-    </label>
-    <label className="block">
-      Definition
-      <textarea
-        value={card.definition}
-        onChange={(e) => onChange(card.__uid, 'definition', e.target.value)}
-        placeholder="Add definition"
-        className="block w-full"
-        rows="1"
-      ></textarea>
-    </label>
-    <button
-      onClick={() => onDelete(card.__uid)}
-      className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-      aria-label="add card"
-    >
-      Delete Card
-    </button>
-  </li>
-)
+import { DeckForm } from '@/components/DeckForm'
 
 const CreateDeckPage = () => {
   const router = useRouter()
-  const [name, setName] = useState('')
-  const [description, setDescription] = useState('')
-  const [cards, setCards] = useState([])
   const [session, loading] = useSession()
 
-  // TODO: improve
   if (!session) {
     return <div>Please Sign In</div>
   }
 
-  // TODO: improve
   if (loading) {
     return <div>Loading...</div>
   }
 
-  const handleChangeCard = (cardId, field, newValue) => {
-    setCards((cardList) =>
-      cardList.map((card) =>
-        card.__uid === cardId ? { ...card, [field]: newValue } : card
-      )
-    )
-  }
-  const handleAddCard = () => {
-    setCards((cardList) => [createEmptyCard(), ...cardList])
-  }
-
-  const handleDeleteCard = (cardId) => {
-    setCards((cardList) => cardList.filter((card) => card.__uid !== cardId))
-  }
-
-  const handleCreateDeck = async () => {
-    const newDeck = { name, description, cards }
+  const handleCreateDeck = async (newDeck) => {
     await axios.post(`/api/decks`, { newDeck })
     router.push('/dashboard')
   }
@@ -86,56 +27,7 @@ const CreateDeckPage = () => {
       <Head>
         <title>MemoWise - Create New Flashcard Set</title>
       </Head>
-      <Container>
-        <p>Create a new flashcard deck</p>
-        <label className="block">
-          Name
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder="Add a name (i.e. Math 101)"
-            className="block w-full"
-          />
-        </label>
-        <label className="block">
-          Description
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            placeholder="Add a description"
-            className="block w-full"
-            rows="3"
-          ></textarea>
-        </label>
-        <button
-          onClick={handleAddCard}
-          className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-          aria-label="add card"
-        >
-          Add Card
-        </button>
-
-        <ul>
-          {cards.map((card) => (
-            <NewCard
-              key={card.__uid}
-              card={card}
-              onChange={handleChangeCard}
-              onDelete={handleDeleteCard}
-            />
-          ))}
-        </ul>
-
-        <button
-          className="inline-flex items-center px-3 py-1.5 text-base font-semibold text-white bg-gray-900 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-          aria-label="add card"
-          onClick={handleCreateDeck}
-        >
-          Create
-        </button>
-      </Container>
-      <Container></Container>
+      <DeckForm submitLabel={'Create'} onSubmit={handleCreateDeck} />
     </>
   )
 }
