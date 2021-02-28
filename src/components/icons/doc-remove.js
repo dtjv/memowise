2e01b7a12276967bae42ec3030ea6d605d37af18
David Valles <davidtjvalles@gmail.com>
2021-02-28 09:04:43 -0800

    Add new icons

---
+++ src/components/icons/doc-remove.js
@@ -0,0 +1,17 @@
+export const DocRemoveIcon = ({ className, ...props }) => (
+  <svg
+    className={`${className}`}
+    {...props}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M9 13h6m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+    ></path>
+  </svg>
+)
---
+++ src/components/icons/plus.js
@@ -0,0 +1,17 @@
+export const PlusIcon = ({ className, ...props }) => (
+  <svg
+    className={`${className}`}
+    {...props}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+    ></path>
+  </svg>
+)
