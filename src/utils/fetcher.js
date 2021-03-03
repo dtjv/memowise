a22d492779f31373c4295775c403d8a1731b4b1b
David Valles <davidtjvalles@gmail.com>
2021-03-03 15:43:06 -0800

    Add error reporting to fetcher

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,10 +1,21 @@
+import axios from 'axios'
+
+import { dump } from '@/utils/debug'
+
 export const fetcher = async (url) => {
-  const res = await fetch(url)
-  const data = await res.json()
+  try {
+    const res = await axios.get(url)
+    return res.data
+  } catch (err) {
+    const error = new Error(err.message ?? `Api Error: ${url}`)
 
-  if (res.status !== 200) {
-    throw new Error(data.message)
+    if (err.response) {
+      error.data = err.response.data
+      error.status = err.response.status
+      error.headers = err.response.headers
+    }
+
+    dump(error)
+    throw error
   }
-
-  return data
 }
