ede5a78a612089882f6d515e16bf2d3fddc298d6
David Valles <davidtjvalles@gmail.com>
2021-09-19 09:43:43 -0700

    Use custom quiz hook

--- src/pages/decks/[deckid]/quiz.js
+++ src/pages/decks/[deckid]/quiz.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from 'react'
+import { useRef, useState, useEffect, useCallback } from 'react'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
 import useSWR from 'swr'
@@ -6,18 +6,9 @@ import arrayShuffle from 'array-shuffle'
 
 import { Container } from '@/components/Container'
 import { BreadCrumbs } from '@/components/BreadCrumbs'
+import { useQuiz } from '@/hooks/useQuiz'
 import { fetcher } from '@/utils/fetcher'
 import { takeRandomItem } from '@/utils/takeRandomItem'
-
-const getNextCard = (cards = [], currentCardId) => {
-  if (!cards.length) {
-    return undefined
-  }
-
-  return currentCardId === undefined
-    ? takeRandomItem(cards)().pop()
-    : takeRandomItem(cards.filter((card) => card.id !== currentCardId))().pop()
-}
 
 const generateChoices = (currentCard, cards = [], options = {}) => {
   const field = options?.field || 'definition'
@@ -65,22 +56,28 @@ const QuizPage = () => {
           },
         ]
       : []
-  const cardRef = useRef(card)
-  cardRef.current = card
+  const {
+    getNextCard,
+    resetQuiz,
+    getScore,
+    markCorrect,
+    markIncorrect,
+    isQuizComplete,
+  } = useQuiz(deck?.cards)
 
   useEffect(() => {
-    setCard(getNextCard(deck?.cards))
+    setCard(getNextCard())
   }, [deck])
 
   useEffect(() => {
-    setChoices(arrayShuffle(generateChoices(cardRef.current, deck?.cards)))
+    setChoices(arrayShuffle(generateChoices(card, deck?.cards)))
     setSelectedChoice(undefined)
   }, [card, deck])
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (selectedChoice) {
-        setCard(getNextCard(deck?.cards, cardRef.current.id))
+        setCard(getNextCard())
       }
     }, 1000)
 
@@ -105,46 +102,90 @@ const QuizPage = () => {
           {deck.description}
         </p>
       </Container>
-      <Container>
-        <div className="mb-8 space-y-6">
-          <div className="p-4 ring-1 ring-black rounded-xl">
-            <span className="text-xs text-gray-500 uppercase">term</span>
-            <div className="flex justify-center mb-4 py-14">
-              <p>{card?.term}</p>
+      {isQuizComplete() ? (
+        <Container>
+          <div className="mb-8 space-y-8">
+            <div className="p-4 ring-1 ring-gray-500 rounded-xl">
+              <div className="flex flex-col items-center py-14 space-y-4">
+                <h3 className="text-2xl font-bold leading-tight text-gray-800">
+                  Quiz Complete!
+                </h3>
+                <div className="flex flex-col items-center space-y-1">
+                  <p className="text-lg font-semibold text-green-500">
+                    Correct
+                  </p>
+                  <p className="text-lg font-bold">{getScore().numCorrect}</p>
+                </div>
+                <div className="flex flex-col items-center space-y-1">
+                  <p className="text-lg font-semibold text-red-500">
+                    Incorrect
+                  </p>
+                  <p className="text-lg font-bold">{getScore().numIncorrect}</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <div
+                className="cursor-pointer inline-flex items-center justify-center rounded-md px-3 py-1.5 font-semibold text-white bg-gray-800 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800"
+                onClick={() => {
+                  resetQuiz()
+                  setCard(getNextCard())
+                }}
+              >
+                Start Over
+              </div>
             </div>
           </div>
-          <ul className="space-y-4">
-            {choices.map((choice, idx) => {
-              const isRightAnswer =
-                selectedChoice?.isCorrectChoice &&
-                selectedChoice?.id === choice.id
-              const isWrongAnswer =
-                !selectedChoice?.isCorrectChoice &&
-                selectedChoice?.id === choice.id
-              const ring = isRightAnswer
-                ? 'ring-2 ring-green-500'
-                : isWrongAnswer
-                ? 'ring-2 ring-red-500'
-                : 'ring-1 ring-gray-300'
+        </Container>
+      ) : (
+        <Container>
+          <div className="mb-8 space-y-6">
+            <div className="p-4 ring-1 ring-black rounded-xl">
+              <span className="text-xs text-gray-500 uppercase">term</span>
+              <div className="flex justify-center mb-4 py-14">
+                <p>{card?.term}</p>
+              </div>
+            </div>
+            <ul className="space-y-4">
+              {choices.map((choice, idx) => {
+                const isRightAnswer =
+                  selectedChoice?.isCorrectChoice &&
+                  selectedChoice?.id === choice.id
+                const isWrongAnswer =
+                  !selectedChoice?.isCorrectChoice &&
+                  selectedChoice?.id === choice.id
+                const ring = isRightAnswer
+                  ? 'ring-2 ring-green-500'
+                  : isWrongAnswer
+                  ? 'ring-2 ring-red-500'
+                  : 'ring-1 ring-gray-300'
 
-              return (
-                <li
-                  key={choice.id}
-                  className={`flex items-center justify-between px-4 py-4 shadow-sm ${ring} rounded-xl hover:shadow-lg cursor-pointer`}
-                  onClick={() => setSelectedChoice(choice)}
-                >
-                  <span>
-                    <span className="mr-3 font-semibold">
-                      {`${String.fromCharCode('A'.charCodeAt() + idx)}`}
+                return (
+                  <li
+                    key={choice.id}
+                    className={`flex items-center justify-between px-4 py-4 shadow-sm ${ring} rounded-xl hover:shadow-lg cursor-pointer`}
+                    onClick={() => {
+                      setSelectedChoice(choice)
+                      if (choice.isCorrectChoice) {
+                        markCorrect()
+                      } else {
+                        markIncorrect()
+                      }
+                    }}
+                  >
+                    <span>
+                      <span className="mr-3 font-semibold">
+                        {`${String.fromCharCode('A'.charCodeAt() + idx)}`}
+                      </span>
+                      {choice.text}
                     </span>
-                    {choice.text}
-                  </span>
-                </li>
-              )
-            })}
-          </ul>
-        </div>
-      </Container>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </Container>
+      )}
     </>
   )
 }
