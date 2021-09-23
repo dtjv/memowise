cd8e04631a8bb20911ea78772226771ac4f6bfa0
David Valles <davidtjvalles@gmail.com>
2021-09-23 10:06:17 -0700

    Add useToggle hook

---
+++ src/hooks/useToggle.js
@@ -0,0 +1,8 @@
+import { useState, useCallback } from 'react'
+
+export const useToggle = (initialState = false) => {
+  const [state, setState] = useState(initialState)
+  const toggle = useCallback(() => setState((state) => !state), [])
+
+  return [state, toggle]
+}
